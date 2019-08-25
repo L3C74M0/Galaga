3ebfe81cf5942569bf7a7ca926578cf66683d821
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2019-08-24 23:11:35 -0500

    Implementado el metodo generar matriz de coeficientes,faltan casos de prueba

--- src/model/Battlefield.java
+++ src/model/Battlefield.java
@@ -11,12 +11,27 @@ public class Battlefield {
 	private int[][] lastBattleMatrix;
 	private int[][] currentBattleMatrix;
 
+	
+	/*
+	 * 
+	 * The constructor method
+	 * 
+	 */
 	public Battlefield() {
 
 	}
-
+	
+	
+	
+	/*
+	 * 
+	 * The method serves to instantiate a past battle matrix and fill it with random values
+	 * @param Int number of rows, number of columns . This to know what size to generate the last battle matrix
+	 * @return The last battle matrix, filled with random integers between 1 and 51
+	 * Pre: Have created an object type BattleField
+	 * 
+	 */
 	public int[][] generateTheLastBattleMatrix(int numberOfRows, int numberOfColumns) {
-
 		Random r = new Random();
 		lastBattleMatrix = new int[numberOfRows][numberOfColumns];
 
@@ -25,13 +40,29 @@ public class Battlefield {
 				lastBattleMatrix[I][J] = r.nextInt(100) + 1;
 			}
 		}
-
 		return lastBattleMatrix;
-
 	}
 	
 	
-	
+	/*
+	 * 
+	 * The method serves to instantiate a coefficient battle matrix and fill it with random values
+	 * @param Int number of rows, number of columns . This to know what size to generate the last battle matrix
+	 * @return The last battle matrix, filled with random integers between 1 and 51
+	 * Pre: Have created an object type BattleField
+	 * 
+	 */
+	public int[][] generateTheCoefficientMatrix(int numberOfRows, int numberOfColumns) {
+		Random r = new Random();
+		coefficientMatrix = new int[numberOfRows][numberOfColumns];
+
+		for (int I = 0; I < coefficientMatrix.length -1; I++) {
+			for (int J = 0; J < coefficientMatrix[I].length -1; J++) {
+				coefficientMatrix[I][J] = r.nextInt(100) + 1;
+			}
+		}
+		return coefficientMatrix;
+	}
 	
 	
 	
